317a71b510bfce1b84db030c3b7cf882c09e64df
Erik Carlson <erikvcarlson@gmail.com>
2021-04-14 19:06:52 -0400

    apr14_1906

--- final_proj_functions.py
+++ final_proj_functions.py
@@ -5,6 +5,8 @@ import datetime as dt
 import time
 import numpy as np
 import multiprocessing as mp
+import navpy 
+import math as m
 
 
 def antenna_puller(antenna_data_csv,antenna_number):
@@ -15,44 +17,58 @@ def antenna_puller(antenna_data_csv,antenna_number):
     return antenna_data
 
 def comparison_loop(gps_sub_data):
-    for m  in range(len(ant_matrix['time'])):
-        for n in range(len(gps_sub_data['time'])):
-            t1 =  ant_matrix['time'][m].to_pydatetime().timestamp()
-            t2 = gps_sub_data['time'][n].to_pydatetime().timestamp()
+    coordinated_data = [];
+    gps_time = gps_sub_data['time'].to_numpy()
+    gps_lat_long_data = gps_sub_data[['Y','X','ele']].to_numpy().tolist()
+    for m  in range(len(ant_matrix)):
+        for n in range(len(gps_time)):
+            t1 =  ant_matrix[m].astype('uint64')/(10**9)
+            t2 = gps_time[n].to_pydatetime().timestamp()
             time_difference = t1 - t2
             if time_difference == 0: 
-                print(time_difference)
+                holding_matrix = gps_lat_long_data[n] + [ant_matrix_rssi[m]]
+                coordinated_data.append(holding_matrix)
+    return coordinated_data
 
 def split_ident(x):
     gps_data_compare = gps_data_break[x]
-    print(x)
     a = comparison_loop(gps_data_compare)
     return a 
 
 def latlong_of_detections_creator(gps_data, antenna_data):
     lat_long_of_detects = pd.DataFrame([])
     global ant_matrix 
+    global ant_matrix_rssi
     global gps_data_break
-    ant_matrix = antenna_data
+    ant_matrix = antenna_data['time'].to_numpy()
+    ant_matrix_rssi =  antenna_data['rssi'].to_numpy().tolist()
+    
     #break the data into chunks and then run multithreaded
-    print(mp.cpu_count())
     gps_split = np.array_split(gps_data,mp.cpu_count())
     gps_data_break = gps_split
-    print('split complete')
     a_pool = mp.Pool(mp.cpu_count())
-    print('pool complete')
     cores = list(range(0,mp.cpu_count()))
-    print(cores)
-    a_pool.map(split_ident,cores)
-    
-    #print(gps_split)
-    #pool_res = [mp.Process]a_pool.map(comparison_loop,gps_split)
-    #for ii, chunk in enumerate(gps_split)
-    #presult = astropy_example.parallel_map(comparison_loop, gps_data)
-    
-    a_pool.close()
-    a_pool.join()
-   # with ProcessPoolExecutor() as executor:
-   #     res = executor.map(comparison_loop,gps_split) 
-        
+    lat_long_of_detects = a_pool.map(split_ident,cores)
+    #combine the chunks into a master list
+    lat_long_of_detects = [ent for sublist in lat_long_of_detects for ent in sublist]       
     return lat_long_of_detects
+
+
+def xyz_converter(station_location,station_offset,detections):
+    xyz_location_1 = []
+    for i in range(len(detections)):
+        source_location_array = detections[i][0:3] 
+        source_location_xyz_offset = navpy.lla2ecef(source_location_array[0],source_location_array[1],source_location_array[2])
+        source_location_xyz = source_location_xyz_offset - station_offset
+        source_location_xyz[2] = source_location_array[2] - station_location[2] 
+        source_location_xyz = np.append(source_location_xyz,detections[i][3])
+        xyz_location_1.append(source_location_xyz.tolist())
+    return xyz_location_1
+
+
+def cart2sph(x,y,z):
+    XsqPlusYsq = x**2 + y**2
+    r = m.sqrt(XsqPlusYsq + z**2)               # r
+    elev = m.atan2(z,m.sqrt(XsqPlusYsq))     # theta
+    az = m.atan2(y,x)                           # phi
+    return r, elev, az
--- raw_python_code.py
+++ raw_python_code.py
@@ -6,6 +6,11 @@ import time
 import numpy as np
 import multiprocessing as mp
 import final_proj_functions as fpf
+import navpy 
+import matplotlib.pyplot as plt
+
+startTime = time.time()
+
 
 gps_data_csv = 'apr7_data.csv'
 
@@ -24,12 +29,63 @@ antenna_3_data = fpf.antenna_puller(antenna_data_csv,3)
 antenna_4_data = fpf.antenna_puller(antenna_data_csv,4)
 antenna_5_data = fpf.antenna_puller(antenna_data_csv,5)
 
-#print(antenna_1_data)
-
 lat_long_of_detections_1 = fpf.latlong_of_detections_creator(gps_data_sorted, antenna_1_data)
-#lat_long_of_detections_2 = latlong_of_detections_creator(gps_data_sorted, antenna_2_data)
-#lat_long_of_detections_3 = latlong_of_detections_creator(gps_data_sorted, antenna_3_data)
-#lat_long_of_detections_4 = latlong_of_detections_creator(gps_data_sorted, antenna_4_data)
-#lat_long_of_detections_5 = latlong_of_detections_creator(gps_data_sorted, antenna_5_data)
-print(lat_long_of_detections_1)
+#lat_long_of_detections_2 = fpf.latlong_of_detections_creator(gps_data_sorted, antenna_2_data)
+#lat_long_of_detections_3 = fpf.latlong_of_detections_creator(gps_data_sorted, antenna_3_data)
+#lat_long_of_detections_4 = fpf.latlong_of_detections_creator(gps_data_sorted, antenna_4_data)
+#lat_long_of_detections_5 = fpf.latlong_of_detections_creator(gps_data_sorted, antenna_5_data)
+
+
+station_location = [41.520752667,-71.599254667,40.0];
+station_offset = navpy.lla2ecef(station_location[0],station_location[1],station_location[2])
+
+ant1_xyz = fpf.xyz_converter(station_location,station_offset,lat_long_of_detections_1)
+#ant2_xyz = fpf.xyz_converter(station_location,station_offset,lat_long_of_detections_2)
+#ant3_xyz = fpf.xyz_converter(station_location,station_offset,lat_long_of_detections_3)
+#ant4_xyz = fpf.xyz_converter(station_location,station_offset,lat_long_of_detections_4)
+#ant5_xyz = fpf.xyz_converter(station_location,station_offset,lat_long_of_detections_5)    
+
+#print(ant1_xyz)
+x_1 = []
+for x in range(0,len(ant1_xyz)):
+    x_1.append(ant1_xyz[x][0])
+
+y_1 = []
+for y in range(0,len(ant1_xyz)):
+    y_1.append(ant1_xyz[y][1])
+
+z_1 = []
+for z in range(0,len(ant1_xyz)):
+    z_1.append(ant1_xyz[z][2])
+
+rssi_1 = []
+for dbm in range(0,len(ant1_xyz)):
+    rssi_1.append(ant1_xyz[dbm][3])
+
+
+
+spherical_coordinates_radius = []
+spherical_coordinates_az = []
+
+for xyz in range(0,len(ant1_xyz)):
+    [radius, ele, az] = fpf.cart2sph(x_1[xyz],y_1[xyz],z_1[xyz])
+    spherical_coordinates_radius.append(radius)    
+    spherical_coordinates_az.append(az)
+    
+    
+plt.plot(spherical_coordinates_radius,rssi_1,'o')
+plt.savefig('foo.png', bbox_inches='tight')
+
+fig = plt.figure()
+ax = fig.add_subplot(111, polar=True)
+ax.plot(spherical_coordinates_az,spherical_coordinates_radius,'o')
+plt.savefig('bar.png', bbox_inches='tight')
+
+
+
+
+executionTime = (time.time() - startTime)
+print('Execution time in seconds: ' + str(executionTime))
+
+#print(lat_long_of_detections_1)
 #for time in antenna_1_data['time']
